06c2de919ad85b2cc4fa6eca4a3589e2c9fc6211
Maelstroms <workpartystudy@yahoo.com>
2017-08-23 13:36:42 -0700

    got dropdown menus to appear, no functionality yet

--- js/components/survey/index.js
+++ js/components/survey/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-import { TouchableOpacity } from "react-native";
+import { TouchableOpacity, DatePickerIOS, TextInput, AppRegistry,  ScrollView,
+  StyleSheet} from "react-native";
+import Menu, { MenuContext, MenuOptions, MenuOption, MenuTrigger } from 'react-native-menu';
 import { connect } from "react-redux";
 import BlankPage2 from "../blankPage2";
 import { DrawerNavigator, NavigationActions } from "react-navigation";
@@ -17,20 +19,31 @@ import {
   Right
 } from "native-base";
 import { Grid, Row } from "react-native-easy-grid";
-//import { setIndex } from "../../actions/list";
+import { setIndex } from "../../actions/list";
 import styles from "./styles";
 
 
 
 class Survey extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: 'Useless Placeholder',
+                    dropdownSelection: '-- Choose --'  };
+  }
   static navigationOptions = {
     header: null
   };
 
-  static numbers = [1, 2, 3, 4, 5];
-  static list = ["Student","Rotation", "Date Performed", "Prodedure Group","Prodedure","Diagnosis Group","Diagnosis", "Case Location","Role in Case", "Supervisor", "Patient Type", "Student Comments", "Alternate Experience", "Description of Alternate Experience"];
+
 
   /*["Student","Rotation", "Date Performed", "Prodedure Group","Prodedure","Diagnosis Group", "Diagnosis", "Case Location", "Role in Case", "Supervisor", "Patient Type", "Student Comments", "Alternate Experience", "Description of Alternate Experience"]
+
+  <Picker
+  selectedValue={this.state.language}
+  onValueChange={(itemValue, itemIndex) => this.setState({language: itemValue})}>
+  <Picker.Item label="Java" value="java" />
+  <Picker.Item label="JavaScript" value="js" />
+</Picker>
 
   setIndex: React.PropTypes.func,
 
@@ -64,17 +77,52 @@ class Survey extends Component {
         <View style={styles.container}>
           <Content>
             <View style={styles.bg}>
+              <MenuContext style={{ flex: 1 }} ref="MenuContext">
+              { /* You need to place a MenuContext somewhere in your application, usually at the root.
+                  Menus will open within the context, and only one menu can open at a time per context.*/}
+                <TextInput
+        style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+        onChangeText={(text) => this.setState({text})}
+        value={this.state.text}/>
+
+
+
+             <Menu style={styles.dropdown} onSelect={(value) => this.setState({ dropdownSelection: value })}>
+            <MenuTrigger>
+              <Text>{this.state.dropdownSelection}</Text>
+            </MenuTrigger>
+            <MenuOptions optionsContainerStyle={styles.dropdownOptions}
+                         renderOptionsContainer={(options) => <ScrollView><Text>CHOOSE SOMETHING....</Text>{options}</ScrollView>}>
+              <MenuOption value="Option One">
+                <Text>Option One</Text>
+              </MenuOption>
+              <MenuOption value="Option Two">
+                <Text>Option Two</Text>
+              </MenuOption>
+              <MenuOption value="Option Three">
+                <Text>Option Three</Text>
+              </MenuOption>
+              <MenuOption value="Option Four">
+                <Text>Option Four</Text>
+              </MenuOption>
+              <MenuOption value="Option Five">
+                <Text>Option Five</Text>
+              </MenuOption>
+            </MenuOptions>
+          </Menu>
+
+
+
+              <TextInput
+        style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+        onChangeText={(text) => this.setState({text})}
+        value={this.state.text}/>
+
+
               {<Grid style={styles.mt}>
                 {this.props.list.map((item, i) => (
-                  <Row key={i}>
-                    <TouchableOpacity
-                      style={styles.row}
-                      onPress={() =>
-                        this.props.navigation.navigate("BlankPage", {
-                          name: { item }
-                        })}>
-                      <Text style={styles.text}>{item}</Text>
-                    </TouchableOpacity>
+                  <Row style ={styles.row} key={i}>
+                    <Text style={styles.text}>{item}</Text>
                   </Row>
                 ))}
               </Grid> }
@@ -88,6 +136,7 @@ class Survey extends Component {
                 onPress={() => this.props.navigation.navigate("Home")}>
                       <Text>Cancel</Text>
                </Button>
+               </MenuContext>
              </View>
           </Content>
         </View>
